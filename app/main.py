fd8a6ee4d89186e5ef88667fbb9e84135c2bf7a6
Johan Espejo <johanm-espejom@outlook.com>
2023-07-30 22:30:14 -0500

    Se agrega fastapi

--- app/main.py
+++ app/main.py
@@ -1,27 +1,32 @@
 import utils
 import readcsv
 import charts as ch
+import pandas as pd
 
 
 
 
 def run():
+  #  data= readcsv.read_csv()
+  # data= list(filter(lambda item : item['Continent'] =='South America',data))
+
+  # countries = list(map(lambda x: x['Country/Territory'],data))
+  # percentages = list(map(lambda x: x['World Population Percentage'],data))
+
+  df = pd.read_csv('data.csv')
+  df = df[df['Continent'] =='Africa']
   data= readcsv.read_csv('data.csv')
-  data= list(filter(lambda item : item['Continent'] =='South America',data))
-
-  countries = list(map(lambda x: x['Country/Territory'],data))
-  percentages = list(map(lambda x: x['World Population Percentage'],data))
-
+  countries = df['Country/Territory'].values
+  percentages = df['World Population Percentage'].values
+  print(countries)
+  print(percentages)
   country=input('Type Country =>')
-
   result = utils.population_by_country(data,country)
   
-
-  if len(result) >0:
-    country=result[0]
-    labels,values= utils.get_population(country)
-    ch.generate_bar_chart(country,labels,values)
-    ch.generate_pie_chart(country,labels,values)
+  country=result[0]
+  labels,values= utils.get_population(country)
+  ch.generate_bar_chart(country,countries,percentages)
+  ch.generate_pie_chart(country,labels,values)
 
 
 
